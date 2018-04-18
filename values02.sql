599018d010c5a23b707a307380d5ca9d9d26bdf8
Ameen Ruhul Mohammad <ameensunny242@gmail.com>
2018-04-18 11:09:06 +0600

    table 2 valuse added

---
+++ values02.sql
@@ -0,0 +1,47 @@
+/* VALUES ('','','','','','','',''); */
+
+INSERT INTO product_master
+	('product_no','description','profit_percent','unit_measure',
+	'qty_on hand','reorder_level','sell_price','cost_price')
+VALUES ('P0001','1.44 Floppies','5','Piece','100','20','525','500');
+
+INSERT INTO product_master
+	('product_no','description','profit_percent','unit_measure',
+	'qty_on hand','reorder_level','sell_price','cost_price')
+VALUES ('P3453','Monitors','6','Piece','10','3','12000','11280');
+
+INSERT INTO product_master
+	('product_no','description','profit_percent','unit_measure',
+	'qty_on hand','reorder_level','sell_price','cost_price')
+VALUES ('P06734','Mouse','5','Piece','20','5','1050','1000');
+
+
+INSERT INTO product_master
+	('product_no','description','profit_percent','unit_measure',
+	'qty_on hand','reorder_level','sell_price','cost_price')
+VALUES ('P07865','1.22 Floppies','5','Piece','100','20','525','500');
+
+INSERT INTO product_master
+	('product_no','description','profit_percent','unit_measure',
+	'qty_on hand','reorder_level','sell_price','cost_price')
+VALUES ('P07868','Keyboard','2','Piece','10','3','3150','3050');
+
+INSERT INTO product_master
+	('product_no','description','profit_percent','unit_measure',
+	'qty_on hand','reorder_level','sell_price','cost_price')
+VALUES ('P07885','CD Drive','2.5','Piece','10','3','5250','5100');
+
+INSERT INTO product_master
+	('product_no','description','profit_percent','unit_measure',
+	'qty_on hand','reorder_level','sell_price','cost_price')
+VALUES ('P07965','540 HDD','4','Piece','10','3','5250','5100');
+
+INSERT INTO product_master
+	('product_no','description','profit_percent','unit_measure',
+	'qty_on hand','reorder_level','sell_price','cost_price')
+VALUES ('P07975','1.44 Drive','5','Piece','10','3','1050','1000');
+
+INSERT INTO product_master
+	('product_no','description','profit_percent','unit_measure',
+	'qty_on hand','reorder_level','sell_price','cost_price')
+VALUES ('P08865','1.22 Drive','5','Piece','2','5','1050','1000');
